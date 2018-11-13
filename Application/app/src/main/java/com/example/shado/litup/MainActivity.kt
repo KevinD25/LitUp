ef92038b498a22d56b2512841b0a0421e5fb55ac
Jorren schepers <s094476@ap.be>
2018-11-13 16:24:25 +0100

    added logout button

--- Application/app/src/main/java/com/example/shado/litup/MainActivity.kt
+++ Application/app/src/main/java/com/example/shado/litup/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.shado.litup
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var currentUser : FirebaseUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_logout.setOnClickListener {
+            logout()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(auth.currentUser != null){
+            currentUser = auth.currentUser as FirebaseUser
+        }
+        else{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    public fun logout(){
+        FirebaseAuth.getInstance().signOut()
     }
 }
