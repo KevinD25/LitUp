5c2eb0e732d8a533fa4c46def7a353c1c918f8a1
Jorren schepers <s094476@ap.be>
2018-12-03 19:26:30 +0100

    added controller and service for getting user info from api

---
+++ LitUp_API/BusinessLayer/UserService.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class UserService
+    {
+        private readonly LitUpContext litUpContext;
+
+        public UserService(LitUpContext litUpContext)
+        {
+            this.litUpContext = litUpContext;
+        }
+    }
+}
---
+++ LitUp_API/LitUp_API/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LitUp_API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/user")]
+    public class UserController : Controller
+    {
+        private readonly UserService userService;
+
+        public UserController(UserService userService)
+        {
+            this.userService = userService;
+        }
+    }
+}
