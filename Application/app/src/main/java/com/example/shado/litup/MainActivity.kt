b8e3b438a26a22bd03b8feb6efb2e9627cfa2c59
Jorren schepers <s094476@ap.be>
2018-12-04 14:02:32 +0100

    added textview and code to show settings

--- Application/app/src/main/java/com/example/shado/litup/MainActivity.kt
+++ Application/app/src/main/java/com/example/shado/litup/MainActivity.kt
@@ -3,20 +3,36 @@ package com.example.shado.litup
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG : String = "Mainactivity"
 
     private lateinit var auth: FirebaseAuth
     private lateinit var currentUser : FirebaseUser
+
+    private val service = RetrofitInstance.getRetrofitInstance().create(LitUpDataService::class.java)
+
+    public lateinit var settings : Settings
+    var disposable :Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         auth = FirebaseAuth.getInstance()
+
+        disposable = service.getSettings(1).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(
+                {result -> fillSettings(result)},
+                {error -> Log.e(TAG, error.message)}
+        )
 
         btn_logout.setOnClickListener {
             logout()
@@ -33,9 +49,40 @@ class MainActivity : AppCompatActivity() {
             startLoginActivity()
     }
 
-    public fun logout(){
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
+    }
+
+    fun logout(){
         FirebaseAuth.getInstance().signOut()
         startLoginActivity()
+    }
+
+    private fun fillSettings(settings : Settings){
+        this.settings = settings
+        var wakeSleep = settings.Wake_SleepTime.split("/")
+        var wake = ""
+        var sleep = ""
+        wakeSleep.forEach { time ->
+            if(time.contains('w')){
+                wake += time.substring(time.indexOf(';'))
+            }
+            if(time.contains('s')){
+                sleep += time.substring(time.indexOf(';'))
+            }
+        }
+        var s = "Brightness: ${settings.Brightness}"
+        wake.split(';').forEach { wakeTime ->
+            if(wakeTime.length > 1)
+                s += "\nWake time: ${wakeTime.substring(wakeTime.indexOf(';') + 1)}"
+        }
+        sleep.split(';').forEach{ sleepTime ->
+            if(sleepTime.length > 1)
+                s += "\nSleep time: ${sleepTime.substring(sleepTime.indexOf(';') + 1)}"
+        }
+        s += "\nLocation: ${settings.Location}\nUnit: ${settings.Unit}"
+        lbl_currentSettings.setText(s)
     }
 
     private fun startLoginActivity(){
