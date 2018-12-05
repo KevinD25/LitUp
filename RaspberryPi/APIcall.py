5f015a592aff1f6a7b1720f11bb5edc00f16e7fa
Unknown <metsstijn@gmail.com>
2018-12-05 20:30:34 +0100

    test

--- RaspberryPi/APIcall.py
+++ RaspberryPi/APIcall.py
@@ -3,20 +3,19 @@ import requests, json, time, os, multiprocessing
 response = requests.get("http://84.197.169.91/LitUp_API/api/weather")
 
 def showGifs(gif):
-	os.system('./led-image-viewer -f /home/pi/apvalley-1819-litup/Rgb-Matrix/rpi-rgb-led-matrix/utils/' + gif + '--led-gpio-mapping="adafruit-hat-pwm" --led-pixel-mapper="Rotate:270" --led-brightness=60')
-
+	pstring = '../Rgb-Matrix/rpi-rgb-led-matrix/utils/led-image-viewer ../Rgb-Matrix/rpi-rgb-led-matrix/utils/' + gif + ' --led-gpio-mapping="adafruit-hat-pwm" --led-pixel-mapper="Rotate:270" --led-brightness=60'
+	os.system(pstring)
+	time.sleep(3)
+	for line in os.popen("ps ax | grep " + pstring + " | grep -v grep"):
+		fields = line.split()
+		pid = fields[0]
+		os.kill(int(pid), signal.SIGKILL)
 data = response.json()
 list = data["list"]
-file = open("weatherData.txt", "w")
 for f in list:
 	print(f["weather"])
 	print(f["weatherDetail"])
 	print(round((f["temp"] - 273), 1))
 	print(f["time"])
-	if f["weather"] == Rain:
-		p = multiprocessing.Process(target=showGifs, name="showGifs", args=("cloud-rain.gif",))
-		p.start()
-		time.sleep(4)
-		p.terminate()
-		p.join
-file.close()
+	if f["weather"] == "Rain":
+		showGifs("cloud-rain.gif")
