39a413998a1c4975814ffdb5c3a53765456c5268
Jorren schepers <s094476@ap.be>
2018-12-08 19:50:56 +0100

    added unit tests for testing valid password

--- Application/app/src/test/java/com/example/shado/litup/RegisterUnitTest.kt
+++ Application/app/src/test/java/com/example/shado/litup/RegisterUnitTest.kt
@@ -2,15 +2,21 @@ package com.example.shado.litup
 
 import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Created by Jorren on 8/12/2018.
  */
 class RegisterUnitTest {
+
+    lateinit var activity : RegisterActivity
+
+    @Before
+    fun setup(){
+        activity = RegisterActivity()
+    }
     @Test
     fun equalPassCorrect(){
-        val activity = RegisterActivity()
-
         var result = activity.checkEqualPass("testPass", "testPass")
 
         assertEquals(true, result)
@@ -18,10 +24,28 @@ class RegisterUnitTest {
 
     @Test
     fun equalPassIncorrect(){
-        val activity = RegisterActivity()
-
         var result = activity.checkEqualPass("TestPass", "testPass")
 
         assertEquals(false, result)
+    }
+
+    @Test
+    fun validatePasswordCorrect(){
+        var correctPasses = arrayListOf<String>("Avalid!Pass123", "other65V@lid", "6Va!LID=", "8Long!P@")
+
+        correctPasses.forEach { s ->
+            var result = activity.checkPassValid("Avalid!Pass123")
+            assertTrue(result)
+        }
+    }
+
+    @Test
+    fun validatePasswordIncorrect(){
+        var correctPasses = arrayListOf<String>("No!Pa3", "unvalidPass!", "veryunvalidpass", "123456789", "otherUnvalidPass")
+
+        correctPasses.forEach { s ->
+            var result = activity.checkPassValid("Avalid!Pass123")
+            assertTrue(result)
+        }
     }
 }
