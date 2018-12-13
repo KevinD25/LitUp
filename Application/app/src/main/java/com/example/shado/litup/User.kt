cafe63779093f45632d7d1775b5cad60b7be2704
Jorren schepers <s094476@ap.be>
2018-12-13 11:22:55 +0100

    added user class to app

---
+++ Application/app/src/main/java/com/example/shado/litup/User.kt
@@ -0,0 +1,10 @@
+package com.example.shado.litup
+
+/**
+ * Created by Jorren on 13/12/2018.
+ */
+class User {
+    lateinit var Id : Number
+    lateinit var FirebaseId : String
+    lateinit var PersonalSettings : Settings
+}
