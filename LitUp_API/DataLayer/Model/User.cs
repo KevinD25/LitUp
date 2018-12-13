7c61657f022a8c0b4232b9a7e49aaef0fa098893
Jorren schepers <s094476@ap.be>
2018-12-13 11:06:54 +0100

    added user class

---
+++ LitUp_API/DataLayer/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirebaseId { get; set; }
+        public Settings PersonalSettings { get; set; }
+
+    }
+}
