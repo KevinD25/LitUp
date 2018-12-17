23f7a86be5ee1cca1cb78738f7411942a70e2fb5
Jorren schepers <s094476@ap.be>
2018-12-17 20:03:09 +0100

    changed user class for retrofit json mapping

--- Application/app/src/main/java/com/example/shado/litup/User.kt
+++ Application/app/src/main/java/com/example/shado/litup/User.kt
@@ -4,7 +4,17 @@ package com.example.shado.litup
  * Created by Jorren on 13/12/2018.
  */
 class User {
-    lateinit var Id : Number
-    lateinit var FirebaseId : String
-    lateinit var PersonalSettings : Settings
+    private lateinit var id : Number
+    private lateinit var firebaseId : String
+    private lateinit var personalSettings : Settings
+
+    var Id : Number
+        get() {return id}
+        set(value) {id = value}
+    var FirebaseId : String
+        get() {return firebaseId}
+        set(value) {firebaseId = value}
+    var PersonalSettings : Settings
+        get() {return personalSettings}
+        set(value) {personalSettings = value}
 }
