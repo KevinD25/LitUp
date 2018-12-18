5ef0e3029f2018fb8f8691dfaca816edba1e5f57
Jorren schepers <s094476@ap.be>
2018-12-18 09:30:57 +0100

    added constructor with firebaseId for user class

--- LitUp_API/DataLayer/Model/User.cs
+++ LitUp_API/DataLayer/Model/User.cs
@@ -6,6 +6,16 @@ namespace DataLayer.Model
 {
     public class User
     {
+        public User()
+        {
+            PersonalSettings = new Settings();
+        }
+
+        public User(string firebaseId)
+        {
+            PersonalSettings = new Settings();
+            FirebaseId = firebaseId;
+        }
         public int Id { get; set; }
         public string FirebaseId { get; set; }
         public Settings PersonalSettings { get; set; }
