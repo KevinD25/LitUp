3ba095a8ab439ed41a9ba14fdc42697c56e82d2d
Unknown <metsstijn@gmail.com>
2018-12-18 14:35:39 +0100

    changed order of the settings file

--- RaspberryPi/APIcall.py
+++ RaspberryPi/APIcall.py
@@ -4,7 +4,9 @@ response = requests.get("http://84.197.169.91/LitUp_API/api/weather")
 playtime = 4
 
 def showGifs(gif):
-	pstring = '../Rgb-Matrix/rpi-rgb-led-matrix/utils/led-image-viewer -t ' + str(playtime) + ' ../Rgb-Matrix/rpi-rgb-led-matrix/utils/' + gif + ' --led-gpio-mapping="adafruit-hat-pwm" --led-pixel-mapper="Rotate:270" --led-brightness=60'
+	f = open("settings.txt", "rt")
+	brightness = f.readline()[12:]
+	pstring = '../Rgb-Matrix/rpi-rgb-led-matrix/utils/led-image-viewer -t ' + str(playtime) + ' ../Rgb-Matrix/rpi-rgb-led-matrix/utils/' + gif + ' --led-gpio-mapping="adafruit-hat-pwm" --led-pixel-mapper="Rotate:270" --led-brightness=' + str(brightness)
 	os.system(pstring)
 data = response.json()
 list = data["list"]
@@ -13,6 +15,6 @@ for f in list:
 	print(f["weatherDetail"])
 	print(round((f["temp"] - 273), 1))
 	print(f["time"])
-	if f["weather"] == "Rain":
-		showGifs("cloud-rain.gif")
+	if f["weather"] == "Clouds":
+		showGifs("cloud-sun.gif")
 		#time.sleep(playtime)
