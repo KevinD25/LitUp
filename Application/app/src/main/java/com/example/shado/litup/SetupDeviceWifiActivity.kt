175623c5783c6494692798772df0fda7c5abbbb2
SanderVOverloop <33574301+SanderVOverloop@users.noreply.github.com>
2019-01-08 12:00:18 +0100

    Added Device setup + wifi

---
+++ Application/app/src/main/java/com/example/shado/litup/SetupDeviceWifiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shado.litup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SetupDeviceWifiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup_device_wifi)
+    }
+}
