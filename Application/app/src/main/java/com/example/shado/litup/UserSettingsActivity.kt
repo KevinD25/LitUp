9154a24d25b12cb6a1e199fcb37fe69d69d9b4a5
Kevin Davis <kevin-davis@hotmail.com>
2019-01-08 15:20:16 +0100

    UserPage 95% styled

--- Application/app/src/main/java/com/example/shado/litup/UserSettingsActivity.kt
+++ Application/app/src/main/java/com/example/shado/litup/UserSettingsActivity.kt
@@ -2,11 +2,17 @@ package com.example.shado.litup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_user_settings.*
 
 class UserSettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_settings)
+        backbutton.setOnClickListener{
+            super.onBackPressed()
+        }
     }
+
+
 }
