d99acd0a191cc7f6b7870bda697441af862bcd5f
Scazi <kevin-davis@hotmail.com>
2019-01-14 19:00:10 +0100

    spinners filled

--- Application/app/src/main/java/com/example/shado/litup/DeviceOverviewActivity.kt
+++ Application/app/src/main/java/com/example/shado/litup/DeviceOverviewActivity.kt
@@ -3,9 +3,13 @@ package com.example.shado.litup
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_device_overview.*
 
 class DeviceOverviewActivity : AppCompatActivity() {
+
+    var Hours = arrayListOf<Int>()
+    var Minutes = arrayListOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +18,8 @@ class DeviceOverviewActivity : AppCompatActivity() {
         btnScreensaver.setOnClickListener{
             goToScreensaver()
         }
+
+        fillSpinners()
     }
 
     private fun goToScreensaver(){
@@ -25,5 +31,20 @@ class DeviceOverviewActivity : AppCompatActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
+
+    private fun fillSpinners(){
+        for (i in 0..23) {
+            Hours.add(i)
+        }
+        for(i in 0..59){
+            Minutes.add(i)
+        }
+
+        spnHourFrom.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, Hours)
+        spnHourTill.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, Hours)
+        spnMinuteFrom.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, Minutes)
+        spnMinuteTill.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, Minutes)
+
     }
 }
